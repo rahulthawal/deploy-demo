4d5d9334e42c4e27da36544adfcf8f3b5fe45215
Rahul A Thawal <rthawal@codecraftworks.com>
2018-04-20 11:31:27 -0400

    ng lint
    Applying ng lint --fix to fix trailing white spaces and other suggested rules.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
